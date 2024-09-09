d0e5828acac684556cf507f91163e3b187878083
Brix John Novesteras <brixjohnovesteras@gmail.com>
2024-09-10 00:36:41 +0800

    dashboard column

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import Dashboard from "@/app/dashboard/page";
 
 export default function Home() {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
-      <h1>Hello World!</h1>
-      </main>
-     
-  );
+  return <Dashboard />;
 }
